c8bfe1e8af2964f6ab7d56337c8bccff6fea4d35
Vojtěch Döme <doev@ikem.cz>
2026-01-16 12:54:26 +0100

    fix: display completion filter text in dropdown menu trigger

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -42,7 +42,7 @@ export function CategoryFilter({
       <DropdownMenu>
         <Button variant="outline" size="sm" asChild>
           <DropdownMenuTrigger>
-            <MoreHorizontalIcon className="size-4" />
+            {completionFilter}<MoreHorizontalIcon className="size-4" />
           </DropdownMenuTrigger>
         </Button>
         <DropdownMenuContent align="end">
